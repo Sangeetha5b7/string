5115ee575a12f9a006b2b5733344e69ba29653b8
Sangeetha5b7 <sangeethathummala9696@gmail.com>
2024-10-29 11:00:04 +0530

    Create StringValue.py

---
+++ StringValue.py
@@ -0,0 +1,12 @@
+'''
+Write a program to get a string value and print the same as output:
+Sample Input:
+Hello World
+Sample Output:
+Hello World
+'''
+# Get a string value from the user
+user_input = input("Enter a string: ")
+
+# Print the same string as output
+print(user_input)
